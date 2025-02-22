49f756141474fff36a422d3cf0d0106e04b7ab31
ljquong <132942951+ljquong@users.noreply.github.com>
2025-02-22 16:26:17 -0700

    Update ghacks_PlotWMap_FullData_v0.py

--- ghacks_PlotWMap_FullData_v0.py
+++ ghacks_PlotWMap_FullData_v0.py
@@ -1,51 +1,4 @@
-import sys
-from os.path import exists
 import matplotlib.pyplot as plt
-
-# CONSTANTS:
-FIRSTARGV = 1
-FIRSTARGV_LEN = 2
-
-def file_input():
-    """
-    file_input, gets filename from command line argument (or prompts if not included)
-    python ghacks_fileopenv0.py recordedv0.txt
-    return: data in inputted file (recorded data)
-    """
-    # Check args, if there is an argument from the command line, get filename or exit
-    if len(sys.argv) > FIRSTARGV:
-        # Get filename
-        filename = sys.argv[FIRSTARGV]
-        if not exists(filename) and len(sys.argv) > FIRSTARGV_LEN:  # multiple arguments, but first argument is not a file
-            print(f"Your first argument does not seem to be a file that exists.")
-            arguments = True # checks whether there are more arguments to be checked
-            while arguments:
-                for i in range(len(sys.argv) - FIRSTARGV_LEN):  # check all arguments to see if they are a file
-                    if exists(sys.argv[i + FIRSTARGV_LEN]):  # if another argument is file, use that
-                        filename = sys.argv[i + FIRSTARGV_LEN]
-                        print(f"But it seems that argument {i + FIRSTARGV_LEN} is a file that exists")
-                        arguments = False
-                    if arguments and i == (len(sys.argv) - FIRSTARGV_LEN - FIRSTARGV):   # when at last file and no file exist --> exit
-                        print (f"Sorry, none of the arguments are a file that exists.")
-                        exit()
-        elif not exists(filename):  # no file exist so exit
-            exit(f"Sorry your file does not exist")
-        elif len(sys.argv) > (FIRSTARGV_LEN): # first argument is a file that exist, but there are too many arguments
-            print(f"Too many arguments. But that's okay because it seems that your first argument is a file that exists....")
-    else:  # No argument is entered in the cmd line so get input filename
-        file_inputted = False
-        while not file_inputted:
-            filename = input(f"Oops no filename provided. Enter filename: ")
-            if exists(filename):
-                file_inputted = True
-                print("ok")
-            else: print("not ok")
-
-    # Open, read and close file
-    file = open(filename)
-    data = file.read()
-    file.close()
-    return data
 
 def plot_position(long_values, lat_values):
     """ Plots given data as longitude as a function of latitude
@@ -66,8 +19,12 @@ def main():
     Main, gets filename from command line argument (or prompts if not included), reads file into dictionary, then executes contact list categorization
     :return: None
     """
+    file = open("updated-data.txt")
+    data = file.read()
+    file.close()
+    
     # Turn file into a list
-    data = file_input().splitlines()  # create a list that separates items per line (\n)
+    data = data.splitlines()  # create a list that separates items per line (\n)
     
     # Make dict from file, list of latitudes and longitudes in the order they were recorded
     data_dict = {}
@@ -86,11 +43,6 @@ def main():
         lat.append(float(data[i][11]))
         long.append(float(data[i][12]))
 
-
-    print(lat)
-    print(long)
-
-        #print(i)
     for i in range(2,len(data),4):  #bestxyz data
         
         data[i] = data[i].split(" ")  # split each space for separated data/field
@@ -99,20 +51,6 @@ def main():
         y.append(float(data[i][8]))
         z.append(float(data[i][9]))
     
-    #print(data_dict)
-
-    #fig, axs = plt.subplots(2)
-    
-    #axs[0].plot(x,y)
-    #axs[1].plot(long, lat)
-    #plt.scatter(x,y,z)
-    #plt.show()
     plot_position(long,lat)
     
-
 main()
-
-"""
-updated-data.txt
-"""
-
