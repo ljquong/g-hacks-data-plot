d943119039b5197dd66ed4b03fc6fec9bae14d5a
fredellapang <127003024+fredellapang@users.noreply.github.com>
2025-02-22 19:27:39 -0700

    Update everythingAllTogether-ghacks.py

--- everythingAllTogether-ghacks.py
+++ everythingAllTogether-ghacks.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 def plot_position(long_values, lat_values, new_long, new_lat):
     """ Plots given data as longitude as a function of latitude
     Parameters: lists of longitude and latitude data
-    Returns: nothing
+    Return: nothing
     """
     img = plt.imread("mapv1.jpg")       # reads in an image of a map of campus that will be used as the background of the plot
     fig, ax = plt.subplots()        # create figure 
@@ -18,66 +18,53 @@ def plot_position(long_values, lat_values, new_long, new_lat):
 
 def smooth(lat, long, v):
     """
-    remove outliers from list of data by averaging 10 data points and removing those that are past the threshold
-    return: list
+    remove outliers, caused by gnss interference, from list of latitudes and longitudes data points by removing those which have velocities that are past the threshold
+    Return: list
     """
-    newlat = lat.copy()
-    newlong = long.copy()
-    counter = 0
-    for i in range(len(v)):
-        if v[i]<6366330 or v[i]>6366334.5:
-            newlat.pop(i-counter)
-            newlong.pop(i-counter)
-            counter+=1
+    newlat = lat.copy()  # generate copy of raw latitude data to be modified
+    newlong = long.copy()   # generate copy of raw longitude data to be modified
+    counter = 0              # counter number of removed items
+    for i in range(len(v)):  # evaluate each velocity
+        if v[i]<6366330 or v[i]>6366334.5:  # if the velocity surpasses threshold within 6366330 and 636634.5, assume gnss interference
+            newlat.pop(i-counter)   # remove latitude value at this instance
+            newlong.pop(i-counter)  # remove latitude value at this instance
+            counter+=1              # increase the counter for removed items
 
-    return newlat, newlong
+    return newlat, newlong          # return new list of processed latitude and longitude data 
 
 def main():
     """
-    Main, gets filename from command line argument (or prompts if not included), reads file into dictionary, then executes contact list categorization
-    :return: None
+    reads data, convert to lists, process data, then display in plot
+    Return: None
     """
-    file = open("updated-data.txt")
-    data = file.read()
-    file.close()
+    file = open("updated-data.txt")  # open updated-data.txt, the file of recorded data
+    data = file.read()     # read the file
+    file.close()           # close the file
     
     # Turn file into a list
     data = data.splitlines()  # create a list that separates items per line (\n)
     
-    # Make dict from file, list of latitudes and longitudes in the order they were recorded
-    data_dict = {}
-
+    # Parse lists of latitudes, longitudes, and velocities in the order they were recorded from data 
     lat = []
     long = []
-    x = []
-    y = []
-    z = []
     v = []
 
-    for i in range(0,len(data),4):  #bestposa data
-        
+    for i in range(0,len(data),4):  # identify the bestposa data
         data[i] = data[i].split(",")  # split each comma separated data/field
-        data_dict[float(data[i][6])-587680] = [float(data[i][11]),float(data[i][12]) ]
-        
-        lat.append(float(data[i][11]))
-        long.append(float(data[i][12]))
+        lat.append(float(data[i][11]))  # append each latitude into lat list
+        long.append(float(data[i][12]))  # append each longitude into long list
 
-    for i in range(2,len(data),4):  #bestxyz data
-        
+    for i in range(2,len(data),4):  # identify the bestxyz data
+
         data[i] = data[i].split(" ")  # split each space for separated data/field
+        x_value = float(data[i][7])  # identify velocity in x value
+        y_value = float(data[i][8])  # identify velocity in y value
+        z_value = float(data[i][9])  # identify velocity in z value
 
-        x_value = float(data[i][7])
-        y_value = float(data[i][8])
-        z_value = float(data[i][9])
-        
-        x.append(float(data[i][7]))
-        y.append(float(data[i][8]))
-        z.append(float(data[i][9]))
-
-        v_value = (x_value**2+y_value**2+z_value**2)**0.5
-        v.append(v_value)
+        v_value = (x_value**2+y_value**2+z_value**2)**0.5 # calculate velocity
+        v.append(v_value)                             # append each velocity into v list
     
-    newlat, newlong = smooth(lat,long,v)
-    plot_position(long,lat,newlong,newlat)
+    newlat, newlong = smooth(lat,long,v)  # process latitude and longitude values using velocity values in smooth function
+    plot_position(long,lat,newlong,newlat)  # display plot of raw values and processed values in plot_position function
     
 main()
